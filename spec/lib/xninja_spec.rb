1e9adcb04f266d2616838025fbb45f75480f2201
oppai <hotsoup.h@gmail.com>
2015-04-09 01:35:01 +0900

    update spec

--- spec/lib/xninja_spec.rb
+++ spec/lib/xninja_spec.rb
@@ -2,8 +2,17 @@ require './spec/spec_helper'
 require './lib/xninja'
 
 describe 'XNinja::Client' do
+  client = XNinja::Client.new
   it 'is not nill' do
-    client = XNinja::Client.new
     expect(client).not_to eq(nil)
+  end
+
+  it 'has some contetns ' do
+    top_contents = client.top
+    expect(top_contents.count > 10).to eq(true)
+    expected_keys = {:title => 'hoge', :url => 'http://hogehoge.com', :thumbnail => 'hugahugahuga'}.keys
+    top_contents.each do |n|
+      expect(n.keys).to eq(expected_keys)
+    end
   end
 end
